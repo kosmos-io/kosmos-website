e9a67053e9e6c76d41b3cb15776fa5b60ea53f22
ONE7live <wangqi_yewu@cmss.chinamobile.com>
2024-02-22 19:46:17 +0800

    fix: sync update static page
    Signed-off-by: ONE7live <wangqi_yewu@cmss.chinamobile.com>

--- docs/assets/js/034cfc3d.1abe949b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkzfile_docs=self.webpackChunkzfile_docs||[]).push([[972],{2080:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);

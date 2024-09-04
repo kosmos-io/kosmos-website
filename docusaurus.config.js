a75c47a7343ad61276baad81293f3a04081b295e
duanmengkk <duanmeng_yewu@cmss.chinamobile.com>
2024-09-04 16:58:43 +0800

    fix bug
    Signed-off-by: duanmengkk <duanmeng_yewu@cmss.chinamobile.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
 
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/website/",
+	baseUrl: "/kosmos-website/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "kosmos", // Usually your GitHub org/user name.
-	projectName: "website", // Usually your repo name.
+	projectName: "kosmos-website", // Usually your repo name.
 	deploymentBranch: "gh-pages",
 	trailingSlash: false,
 
